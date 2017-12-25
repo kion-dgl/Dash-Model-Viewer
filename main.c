8672e73129c2bac4fb25dd171d9aa7bf6b96fb8a
Kion <kion@dashgl.com>
2017-12-25 22:42:18 +0900

    layout fixes

--- main.c
+++ main.c
@@ -1,12 +1,17 @@
+#include <epoxy/gl.h>
+#include <epoxy/glx.h>
 #include <gtk/gtk.h>
+
+static void on_realize(GtkGLArea *gl_area);
+static void on_render(GtkGLArea *gl_area, GdkGLContext *context);
 
 int main(int argc, char *argv[]) {
 
 	GtkWidget *window;
 	GtkWidget *header;
-	GtkWidget *hbox, *vbox, *rbox;
-	GtkWidget *list_frame, *lod_frame, *gl_frame;
-	GtkWidget *radio_low, *radio_med, *radio_high;
+	GtkWidget *hbox;
+	GtkWidget *open_file;
+	GtkWidget *list_frame, *gl_frame;
 	GtkWidget *listbox, *scrolled_window;
 	GtkWidget *gl_area;
 
@@ -25,6 +30,9 @@ int main(int argc, char *argv[]) {
 	gtk_header_bar_set_title(GTK_HEADER_BAR(header), "Dash Model Viewer");
 	gtk_header_bar_set_show_close_button(GTK_HEADER_BAR(header), TRUE);
 	gtk_window_set_titlebar(GTK_WINDOW(window), header);
+
+	open_file = gtk_file_chooser_button_new("Open", GTK_FILE_CHOOSER_ACTION_OPEN);
+	gtk_header_bar_pack_start(GTK_HEADER_BAR(header), open_file);
 
 	// Create horizontal box and add to Window
 
@@ -48,41 +56,6 @@ int main(int argc, char *argv[]) {
 	gtk_container_add(GTK_CONTAINER(scrolled_window), listbox);
 	gtk_scrolled_window_set_min_content_width(GTK_SCROLLED_WINDOW(scrolled_window), 80);
 
-	// Create Vertical Box and add to Window
-	
-	/*
-
-	vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-	gtk_box_pack_start(GTK_BOX(hbox), vbox, TRUE, TRUE, 0);
-
-	// Create Level of Detail Frame
-	
-	lod_frame = gtk_frame_new("Level of Detail");
-
-	radio_low = gtk_radio_button_new_with_label(NULL, "Low");
-	radio_med = gtk_radio_button_new_with_label(NULL, "Medium");
-	radio_high = gtk_radio_button_new_with_label(NULL, "High");
-
-	gtk_radio_button_join_group(GTK_RADIO_BUTTON(radio_med), GTK_RADIO_BUTTON(radio_low));
-	gtk_radio_button_join_group(GTK_RADIO_BUTTON(radio_high), GTK_RADIO_BUTTON(radio_low));
-
-	gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON(radio_high), TRUE);
-	gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON(radio_med), FALSE);
-	gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON(radio_low), FALSE);
-
-	rbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
-	gtk_box_pack_start(GTK_BOX(vbox), rbox, FALSE, FALSE, 0);
-	
-	gtk_widget_set_halign(GTK_WIDGET(radio_high), GTK_ALIGN_CENTER);
-	gtk_widget_set_halign(GTK_WIDGET(radio_med), GTK_ALIGN_CENTER);
-	gtk_widget_set_halign(GTK_WIDGET(radio_low), GTK_ALIGN_CENTER);
-
-	gtk_box_pack_start(GTK_BOX(rbox), radio_high, TRUE, TRUE, 0);
-	gtk_box_pack_start(GTK_BOX(rbox), radio_med, TRUE, TRUE, 0);
-	gtk_box_pack_start(GTK_BOX(rbox), radio_low, TRUE, TRUE, 0);
-	
-	*/
-
 	// Create GLArea Frame
 
 	gl_frame = gtk_frame_new(NULL);
@@ -91,6 +64,8 @@ int main(int argc, char *argv[]) {
 	gl_area = gtk_gl_area_new();
 	gtk_widget_set_vexpand(gl_area, TRUE);
 	gtk_widget_set_hexpand(gl_area, TRUE);
+	g_signal_connect(GTK_WIDGET(gl_area), "realize", G_CALLBACK(on_realize), NULL);
+	g_signal_connect(GTK_WIDGET(gl_area), "realize", G_CALLBACK(on_render), NULL);
 	gtk_container_add(GTK_CONTAINER(gl_frame), gl_area);
 
 	// Show all widgets and start program
@@ -99,5 +74,22 @@ int main(int argc, char *argv[]) {
 	gtk_main();
 
 	return 0;
+
+}
+
+static void on_realize(GtkGLArea *gl_area) {
+
+	g_print("On Realize\n");
+
+	gtk_gl_area_make_current(gl_area);
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+}
+
+static void on_render(GtkGLArea *gl_area, GdkGLContext *context) {
+
+	g_print("On render\n");
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 }
