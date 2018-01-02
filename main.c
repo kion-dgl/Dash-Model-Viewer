cd18d6b73a8bfc066f963e05e3aa9bcc46826977
Kion <kion@dashgl.com>
2018-01-02 14:56:15 +0900

    fmemopen

--- main.c
+++ main.c
@@ -1,16 +1,25 @@
+#include <stdint.h>
+#include <stdlib.h>
 #include <epoxy/gl.h>
 #include <epoxy/glx.h>
 #include <gtk/gtk.h>
 
 static void on_realize(GtkGLArea *gl_area);
 static void on_render(GtkGLArea *gl_area, GdkGLContext *context);
+static void open_file_click(GtkButton *button, gpointer data);
+static void copy_bin_file(char *filename);
+
+struct {
+	FILE *fp;
+	uint32_t file_len;
+} global;
 
 int main(int argc, char *argv[]) {
 
 	GtkWidget *window;
 	GtkWidget *header;
 	GtkWidget *hbox;
-	GtkWidget *open_file;
+	GtkWidget *open_file, *export_file;
 	GtkWidget *list_frame, *gl_frame;
 	GtkWidget *listbox, *scrolled_window;
 	GtkWidget *gl_area;
@@ -31,8 +40,12 @@ int main(int argc, char *argv[]) {
 	gtk_header_bar_set_show_close_button(GTK_HEADER_BAR(header), TRUE);
 	gtk_window_set_titlebar(GTK_WINDOW(window), header);
 
-	open_file = gtk_file_chooser_button_new("Open", GTK_FILE_CHOOSER_ACTION_OPEN);
+	open_file = gtk_button_new_with_label("Open");
 	gtk_header_bar_pack_start(GTK_HEADER_BAR(header), open_file);
+	g_signal_connect(GTK_WIDGET(open_file), "clicked", G_CALLBACK(open_file_click), (gpointer)window);
+
+	export_file = gtk_button_new_with_label("Export");
+	gtk_header_bar_pack_end(GTK_HEADER_BAR(header), export_file);
 
 	// Create horizontal box and add to Window
 
@@ -91,5 +104,80 @@ static void on_render(GtkGLArea *gl_area, GdkGLContext *context) {
 	g_print("On render\n");
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+}
+
+
+static void open_file_click(GtkButton *button, gpointer data) {
+
+	GtkWidget *dialog;
+	gint res;
+
+	dialog = gtk_file_chooser_dialog_new(
+		"Open File (.BIN)",
+		GTK_WINDOW(data),
+		GTK_FILE_CHOOSER_ACTION_OPEN,
+		"Cancel",
+		GTK_RESPONSE_CANCEL,
+		"Open",
+		GTK_RESPONSE_ACCEPT,
+		NULL
+	);
+
+	res = gtk_dialog_run(GTK_DIALOG(dialog));
+
+	if(res == GTK_RESPONSE_ACCEPT) {
+		
+		char *filename;
+		GtkFileChooser *chooser = GTK_FILE_CHOOSER(dialog);
+		filename = gtk_file_chooser_get_filename(chooser);
+		g_print("Filename: %s\n", filename);
+		copy_bin_file(filename);
+		g_free(filename);
+
+	}
+	
+	gtk_widget_destroy(dialog);
+
+}
+
+
+static void copy_bin_file(char *filename) {
+
+	// Clear previous file (if exists)
+
+	if(global.fp != NULL) {
+		fclose(global.fp);
+	}
+
+	/*
+	* TODO : Clear list box
+	*/
+
+	// Open new file
+
+	FILE *fp = fopen(filename, "r");
+	if(fp == NULL) {
+		fprintf(stderr, "Could not open %s for reading\n", filename);
+		return;
+	}
+	
+	// Get file length
+
+	fseek(fp, 0, SEEK_END);
+	global.file_len = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+
+	// Read file as buffer
+
+	uint8_t *buffer;
+	buffer = malloc(global.file_len);
+	fread(buffer, sizeof(uint8_t), global.file_len, fp);
+	global.fp = fmemopen(buffer, global.file_len, "r");
+
+	// Free memory and close file handler
+
+	free(buffer);
+	fclose(fp);
 
 }
